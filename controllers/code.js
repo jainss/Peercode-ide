e622229b9934091c5bc83972e38616e9be6c7e79
AnkitaMishra945 <94731310+AnkitaMishra945@users.noreply.github.com>
2023-02-01 14:44:16 +0530

    Deploying Project

---
+++ controllers/code.js
@@ -0,0 +1,53 @@
+const Code = require("../models/Code");
+const { uuid } = require("uuidv4");
+
+exports.uploadCode = async (req, res) => {
+  try {
+    const { code, extension, language } = req.body;
+    const newCode = new Code({ code, extension, language });
+    newCode.save();
+    res.status(201).send(newCode._id);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Something went wrong!" });
+  }
+};
+
+exports.getCode = async (req, res) => {
+  try {
+    const { codeId } = req.params;
+    const code = await Code.findOne({ _id: codeId });
+    if (!code) {
+      res.status(400).json({ error: "Invalid URL!" });
+    } else res.status(200).send(code);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Something went wrong!" });
+  }
+};
+
+//Create room
+exports.createRoom = async (req, res) => {
+  try {
+    const code = new Code({ username: req.body.username });
+    await code.save();
+    res.status(201).send(code);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+exports.createRooms = async (req, res) => {
+  try {
+    let arr = [];
+    for (let i = 0; i < req.body.numberRooms; i++) {
+      arr.push({
+        username: uuid(),
+      });
+    }
+    const data = await Code.insertMany(arr);
+    res.status(201).send({ data });
+  } catch (error) {
+    console.log(error);
+  }
+};
